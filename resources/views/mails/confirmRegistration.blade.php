093e29beb3c20ad95a85217f60166199c945dc2c
soniaodea <soniaodea@gmail.com>
2019-01-14 21:29:30 +0100

    mailConstruction2

--- resources/views/mails/confirmRegistration.blade.php
+++ resources/views/mails/confirmRegistration.blade.php
@@ -14,7 +14,7 @@
         {{--
         Tu dirección de correo {{ $email }} ha sido registrada en nuestra aplicación {{ config("app.url") }}. Pulsa sobre <a href="{{ route( "registerEmailConfirmation",["token" => $confirmLink]) }}">este enlace</a> o copia la siguiente url en tu navegador <a href="{{ route( "registerEmailConfirmation",["token" => $confirmLink]) }}">{{ route( "registerEmailConfirmation",["code" => $confirmLink]) }}</a> para activar tu cuenta.-->
         --}}
-        Tu dirección de correo {{ $email }} ha sido registrada en nuestra aplicación {{ config("app.url") }}. Pulsa sobre kkdelavaca para activar tu cuenta.-->
+        Tu dirección de correo ... Pulsa sobre kkdelavaca para activar tu cuenta.-->
     </p>
 </body>
 </html>
